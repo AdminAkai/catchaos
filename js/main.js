5cf5dc2dea2ee612c3f1f25fb4475e0ef1792e35
JCaptain <joshietrinidad@gmail.com>
2019-10-03 09:48:29 -0400

    pixelcat animation testing

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
-let myAnimation = anime({
-  targets: '.main-content',
-  rotate: [60, -60],
-  backgroundColor: ['#FFFFFF', '#FFFFFF'],
-  easing: 'linear',
-  duration: 2000,
-  loop: true,
-  direction: 'alternate'
+
+
+let enemyAnim = anime({
+  targets: '.pixelcat',
+  translateX: {
+    value: 250,
+    duration: 800,
+    direction: 'alternate'
+  },
+  rotate: {
+    value: 360,
+    duration: 1800,
+    easing: 'easeInOutSine',
+    direction: 'alternate'
+  },
+  scale: {
+    value: 2,
+    duration: 1600,
+    delay: 800,
+    easing: 'easeInOutQuart',
+    direction: 'alternate'
+  },
+  delay: 250 // All properties except 'scale' inherit 250ms delay
 });
