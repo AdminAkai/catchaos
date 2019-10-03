75cbfabd50db5d3bc8eb9da2246f68a1f10583fa
JCaptainnnn <49946613+JCaptainnnn@users.noreply.github.com>
2019-10-03 00:37:04 -0400

    Testing animejs using portfolio template as a base

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let myAnimation = anime({
+  targets: '.main-content',
+  rotate: [60, -60],
+  backgroundColor: ['#FFFFFF', '#FFFFFF'],
+  easing: 'linear',
+  duration: 2000,
+  loop: true,
+  direction: 'alternate'
+});
