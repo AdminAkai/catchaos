cffce0b8f5fe902a5db3e40b4f4d821d864844cc
JCaptain <joshietrinidad@gmail.com>
2019-10-04 10:26:22 -0400

    Starting game over and game start logic

--- js/main.js
+++ js/main.js
@@ -3,30 +3,42 @@ var game = {
   points: 0,
   highScore: 0,
   timer: 3000, //in milliseconds
-  spawnrate: 2,
+  spawnrate: 1,
   enemyCount: 0
 }
 
 function gameStart () {
-  
+  let startGame = document.getElementById("game-start-button")
+  startGame.addEventListener('click', clickGameStart
+} 
+
+function clickGameStart () {
+  this.parentNode.removeChild(this)
+  game.gameRun = true;
+}
+
+function gameOver () {
+
 }
 
 function getRandomPosition(element) {
-	var x = document.body.offsetHeight-element.clientHeight;
-	var y = document.body.offsetWidth-element.clientWidth;
-	var randomX = Math.floor(Math.random()*x);
-	var randomY = Math.floor(Math.random()*y);
+  let parentElement = document.getElementsByClassName("game-space")[0]
+	var x = parentElement.offsetHeight-element.clientHeight - 300;
+	var y = parentElement.offsetWidth-element.clientWidth - 100;
+	var randomX = Math.floor(Math.random()*x) + 100;
+	var randomY = Math.floor(Math.random()*y) + 300;
 	return [randomX,randomY];
 }
-// window.onload = function() {
-// 	var img = document.createElement('img');
-// 	img.setAttribute("style", "position:absolute;");
-// 	img.setAttribute("src", "some-image.jpg");
-// 	document.body.appendChild(img);
-// 	var xy = getRandomPosition(img);
-// 	img.style.top = xy[0] + 'px';
-// 	img.style.left = xy[1] + 'px';
-// }
+
+function spawnHeart () {
+  let enemyElement = document.createElement('img')
+  enemyElement.src = 'assets/pixelcat.png'
+  var xy = getRandomPosition(enemyElement)
+  enemyElement.style.top = xy[0] + 'px'
+  enemyElement.style.left = xy[1] + 'px'
+  enemyElement.className = 'pixelcat' 
+}
+
 
 function spawnEnemy() {
   for (let i = 0; i < game.spawnrate; i++) {
@@ -49,44 +61,32 @@ function spawnEnemy() {
     targets: document.querySelectorAll('.pixelcat'),
     direction: 'alternate',
     scale: {
-      value: 2,
+      value: 1.5,
       duration: 800,
       delay: 0,
       easing: 'easeOutQuad',
-    }, // All properties except 'scale' inherit 250ms delay
+    }, 
     rotate: {
       value: 360,
       duration: 800,
-      easing: 'easeOutQuad',
-
+      easing: 'easeOutQuad',  
     },
   });
   game.enemyCount++
   game.timer -= 50
   console.log(game.timer)
 }
-//
-
-  // for (var x = 0; x < 3; x++) {
-  //     for (var i = 0; i < cards.length; i++) {
-  //         var cardElement = document.createElement('img');
-  //         cardElement.className = "cards";
-  //         cardElement.setAttribute('src', 'images/back.png');
-  //         cardElement.setAttribute('data-id', i);
-  //         cardElement.addEventListener('click', flipCard);
-  //         document.getElementById('game-board').appendChild(cardElement);
-  //     }
-  // }
 
 function clickEnemy () {
-  // enemyId = this.getAttribute('data-id') 
   game.points += 10
   this.parentNode.removeChild(this)
   console.log(game.points)
 }
 
-// while (game.gameRun === true) {
-//let enemySpawn = setInterval(spawnEnemy, game.timer)
-// }
+gameStart()
+
+while (game.gameRun === true) {
+  let enemySpawn = setInterval(spawnEnemy, game.timer)
+}
 
 
